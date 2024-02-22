eead1ef9de2058c68bc7b0ff10d1d0beb9d2649e
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-02-22 08:29:23 +0200

    corrected switcher

--- src/components/Switcher/index.tsx
+++ src/components/Switcher/index.tsx
@@ -2,16 +2,16 @@ import React, {useState} from "react";
 import "./style.scss"
 
 export const Switcher = () => {
-    const[forInividuals, forBusiness] = useState(true);
-    const[Inividuals, Business] = useState(true);
+    const[forIndividuals, forBusiness] = useState(true);
+    const[Individuals, Business] = useState(true);
     const handleClick = () => {
-        forBusiness(!forInividuals);
-        Business(!Inividuals);
+        forBusiness(!forIndividuals);
+        Business(!Individuals);
     }
     return (
         <div className = "switcher">
-            <button className ={forInividuals?"active":""} onClick={handleClick}>For Individuals</button>
-            <button className ={Inividuals?"":"active"} onClick={handleClick}>For Business</button>
+            <button className ={forIndividuals?"active":""} onClick={handleClick}>For Individuals</button>
+            <button className ={Individuals?"":"active"} onClick={handleClick}>For Business</button>
         </div>
     );
 }
